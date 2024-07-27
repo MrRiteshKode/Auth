f4c5bbff2bee82d0cf6d136d5eb98154a6ca4aaa
Ritesh <riteshkumar05surat@gmail.com>
2024-07-27 19:06:48 +0530

    check

--- gallery/admin.py
+++ gallery/admin.py
@@ -6,7 +6,7 @@ from .models import Image, About
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
 	list_display = ['id', 'photo','user','date']
-
+	
 @admin.register(About)
 class AboutAdmin(admin.ModelAdmin):
 	list_display = ['id', 'fullName','about','user']
